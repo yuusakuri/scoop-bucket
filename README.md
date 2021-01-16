dc1709a436acad4c69dacd84cfe195b16f1b0b7b
yuusakuri <yuusakurip1@gmail.com>
2021-01-16 17:09:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Scoop Bucket
+
+My [Scoop](https://github.com/lukesampson/scoop) bucket.
+
+## Add Bucket
+
+To install the app from this bucket, first execute the following code in PowerShell.
+
+```powershell
+scoop bucket add yuusakuri https://github.com/yuusakuri/scoop-bucket.git
+```
+
+## Install apps
+
+Execute the following code to install the app.
+
+```powershell
+scoop install APPNAME
+```
---
+++ scripts/uninstall_fonts.ps1
@@ -0,0 +1,14 @@
+﻿if (!(is_admin)) { error "Admin rights are required."; exit 1 
+}
+$uninstallFontFileRegex = (Get-ChildItem -LiteralPath $dir -Recurse -File |
+    Where-Object Name -Match '\\.(otf|ttf)$' |
+    Select-Object -ExpandProperty BaseName) -join '|'
+
+Get-Fonts -WarningAction SilentlyContinue |
+Where-Object { $_.File.Name -match $uninstallFontFileRegex } |
+Select-Object -ExpandProperty Name |
+ForEach-Object { PSWinGlue\Uninstall-Font -Name $_ -Scope System }
+<# Get-Fonts -WarningAction SilentlyContinue |
+Where-Object { $_.File.Name -match $uninstallFontFileRegex } |
+Select-Object -ExpandProperty Name |
+ForEach-Object { PSWinGlue\Uninstall-Font -Name $_ -Scope User } #>
