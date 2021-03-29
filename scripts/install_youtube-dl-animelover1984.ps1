6f98f146567580d4ee3de27aaa306258b6fd40b0
yuusakuri <yuusakurip1@gmail.com>
2021-03-30 03:50:29 +0900

    Fix install script of youtube-dl-animelover

--- scripts/install_youtube-dl-animelover1984.ps1
+++ scripts/install_youtube-dl-animelover1984.ps1
@@ -3,7 +3,13 @@ python3.exe -m pip install requests mutagen websockets git+https://github.com/an
 Get-Command -Name "pip.exe" |
 Select-Object -ExpandProperty Source |
 Split-Path -Parent |
-Join-Path -ChildPath "youtube-dl.exe" |
-Rename-Item -LiteralPath { $_ } -NewName "youtube-dl-animelover1984.exe"
+Get-ChildItem -LiteralPath { $_ } -File |
+Where-Object { $_.Name -in "youtube-dl.exe", "youtube-dl-script.py" } |
+ForEach-Object {
+    $aRenameFile = $_
+    $aNewName = $aRenameFile.Name -replace 'youtube-dl', 'youtube-dl-animelover1984'
+
+    Rename-Item -LiteralPath $aRenameFile.FullName -NewName $aNewName
+}
 
 Write-Host "The exe file name is youtube-dl-animelover1984.exe."
